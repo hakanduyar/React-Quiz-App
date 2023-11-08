7b06c584ef7ce38043ef8656b7af319a821b9700
rikords <iamhakanduyar@gmail.com>
2023-11-08 21:28:28 +0300

    update

--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,9 @@
 const shuffleArray = (array) => {
-  return [...array].sort(() => Math.random(-0.5));
+  return [...array].sort(() => Math.random() - 0.5);
 };
 
 export const fetchQuizData = async (difficulty, amount) => {
-  const url = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}`;
+  const url = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
   const data = await (await fetch(url)).json();
   return data.results.map((dt) => ({
     ...dt,
