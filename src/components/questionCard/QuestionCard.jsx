3088cd78b113a411ceb1e5dd6c430c4a0c16051a
rikords <iamhakanduyar@gmail.com>
2023-11-08 21:36:34 +0300

    update

--- src/components/questionCard/QuestionCard.jsx
+++ src/components/questionCard/QuestionCard.jsx
@@ -10,11 +10,20 @@ const QuestionCard = ({
   modal,
   setModal,
 }) => {
+  const approvedChoice = (e) => {
+    console.log(e.currentTarget.value);
+  };
+
   return (
     <div className="questionCard">
       <div>
         {count + 1}/10 - {questionData[count]?.question}
       </div>
+      {questionData[count]?.answers?.map((answer, i) => (
+        <button onClick={approvedChoice} key={i} value={answer}>
+          {answer}
+        </button>
+      ))}
     </div>
   );
 };
