0fabde3c9e856d194a7edc57b47b170d45b1917e
rikords <iamhakanduyar@gmail.com>
2023-11-10 00:41:28 +0300

    update

--- src/pages/introduce/Introduce.jsx
+++ src/pages/introduce/Introduce.jsx
@@ -22,7 +22,12 @@ const Introduce = () => {
         <img
           src="https://scontent.fist6-1.fna.fbcdn.net/v/t39.30808-6/266063662_547905400024321_545818210580367969_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=5f2048&_nc_ohc=6S8xgEW_SAEAX_TbnU4&_nc_ht=scontent.fist6-1.fna&oh=00_AfAf5mIPzjrD7RPFTuuz6U_5hmnN6C7Sy9czak69pkK49g&oe=654F138D"
           alt=""
-          style={{ width: "300px", height: "200px", borderRadius: "10px" }}
+          style={{
+            width: "300px",
+            height: "200px",
+            border: "1.5px solid black",
+            borderRadius: "10px",
+          }}
         />
 
         <Dropdown data={difficulty} setDifficultyChange={setDifficultyChange} />
