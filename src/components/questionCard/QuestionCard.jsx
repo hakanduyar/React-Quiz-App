3a1f9369058870e68ac10a19dfc12f70b363b524
rikords <iamhakanduyar@gmail.com>
2023-11-09 23:45:06 +0300

    update

--- src/components/questionCard/QuestionCard.jsx
+++ src/components/questionCard/QuestionCard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./QuestionsCard.css";
 
 const QuestionCard = ({
@@ -17,10 +17,34 @@ const QuestionCard = ({
     const checkAnswer =
       e.currentTarget.value == questionData[count]?.correct_answer;
     console.log(checkAnswer);
+    if (checkAnswer) {
+      setScore(score + 10);
+    }
+    setCount(count + 1);
+    if (count == 9) setModal(true);
+    setTimer(30);
   };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (timer > 0) {
+        setTimer(timer - 1);
+      }
+      if (timer == 0 && count < 10) {
+        setCount(count + 1);
+        setTimer(30);
+      } else if (count >= 10) {
+        setModal(true);
+      }
+    }, 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [timer]);
 
   return (
     <div className="questionCard">
+      <div className="questionCard-timer">{timer}</div>
       <div className="questionCard-title">
         {count + 1}/10 - {questionData[count]?.question}
       </div>
